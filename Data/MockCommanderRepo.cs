b7fb112d97948c4dabaa9c3fd6b06fe8e7e5ce66
John Doe <johndoe@email.com>
2022-03-07 21:26:48 +0100

    Problem: added interface and i can't get using Commander.Models

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,15 @@
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public System.Collections.Generic.IEnumerable<Command> GetAppCommands()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
