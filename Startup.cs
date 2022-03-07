25805de5b2144da704c4f120ae344eb2852d6893
John Doe <johndoe@email.com>
2022-03-07 20:29:52 +0100

    Added Command.cs inside Models folder with 4 properties

--- Startup.cs
+++ Startup.cs
@@ -23,9 +23,7 @@ namespace Commander
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-
-        //proba
+       
         public void ConfigureServices(IServiceCollection services)
         {
 
