c31a1d404ffef9fa952eee8b3198630f5f47c1b6
John Doe <johndoe@email.com>
2022-03-07 22:00:51 +0100

    Corrected automaticaly created errors... added return and list in Mock..cs

--- Data/MockCommanderRepo.cs
+++ Data/MockCommanderRepo.cs
@@ -1,17 +1,25 @@
+using System.Collections.Generic;
 using Commander.Models;
 
 namespace Commander.Data
 {
     public class MockCommanderRepo : ICommanderRepo
     {
-        public System.Collections.Generic.IEnumerable<Command> GetAppCommands()
+        public IEnumerable<Command> GetAppCommands()
         {
-            throw new System.NotImplementedException();
+            var commands = new List<Command>
+            {
+                new Command{Id=0, HowTo="Boil an eggr", Line="Boil water", Platform="Kettle and Pan"},
+                new Command{Id=0, HowTo="Cut bread", Line="Get a knife", Platform="Knife"},
+                new Command{Id=0, HowTo="Make cup of tea", Line="Place teabag and a cup", Platform="Kettle and Cup"}
+            };
+
+            return commands;
         }
 
         public Command GetCommandById(int id)
         {
-            throw new System.NotImplementedException();
+            return new Command{Id=0, HowTo="Boil an eggr", Line="Boil water", Platform="Kettle and Pan"};
         }
     }
 }
